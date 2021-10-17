95185c316786397ce1998710d1b29e42c75d33b7
Cobular <22972550+JakeCover@users.noreply.github.com>
2021-10-17 00:02:55 -0700

    Add meta description tag to the index

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,6 +2,10 @@
   import ImageModalGrid from '../components/Grid/ImageModalGrid.svelte'
 </script>
 
+<svelte:head>
+  <meta name='description' content='A gallery of some of my works!'/>
+</svelte:head>
+
 <main>
   <ImageModalGrid />
 </main>
